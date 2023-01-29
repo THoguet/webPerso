e341dd316a569ac760e51d2e17594b02d2cd2158
TO <th.theophyl.hoguet@gmail.com>
2023-01-29 21:30:27 +0100

    backend

---
+++ backend/src/test/java/fr/nessar/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.nessar.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
